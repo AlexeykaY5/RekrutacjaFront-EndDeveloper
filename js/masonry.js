8f15ee725aa736eef96765c426d11179d99b786a
AlexeykaY5 <alexsosnovski21@gmail.com>
2023-08-21 23:14:14 +0200

    end

--- js/masonry.js
+++ js/masonry.js
@@ -1,5 +1,15 @@
 var elem = document.querySelector('.grid');
+let GutterWidth = 40
+
+if (window.innerWidth <= 425) {
+  GutterWidth = 10
+} else if(window.innerWidth <= 769){
+  GutterWidth = 20
+}else if(window.innerWidth >= 769){
+  GutterWidth = 40
+}
+
 var msnry = new Masonry( elem, {
   itemSelector: '.grid_item',
-  gutter: 40
+  gutter: GutterWidth
 });
--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
-let search_btn = document.getElementById("search_btn");
-let nav_input = document.getElementById("nav_input");
+const search_btn = document.getElementById("search_btn");
+const nav_input = document.getElementById("nav_input");
 
 function toggleActiveClass(event) {
     event.preventDefault();
@@ -8,19 +8,45 @@ function toggleActiveClass(event) {
 
 search_btn.addEventListener("click", toggleActiveClass);
 
-let rozwin_btn = document.getElementById("rozwin_btn");
-let grid__block = document.getElementById("grid__block");
-let open_grid = document.getElementById("open_grid");
+const images = document.querySelectorAll('.grid_item');
+
+images.forEach(image => {
+    const modal_block = document.getElementById('modal_block');
+    const modal_content = document.getElementById('modal_content');
+    const modal_text = document.getElementById('modal_text');
+    const close_button = document.getElementById('close_button');    
+    const darkness = document.getElementById('darkness'); 
+    image.addEventListener('click', () => {
+        darkness.classList.add("active");
+        modal_block.style.display = 'block';
+        modal_content.src = image.querySelector('img').src;
+        modal_text.textContent = image.textContent;
+        modal_text.style.display = 'block';
+    });
+
+    darkness.addEventListener('click', () => {
+        darkness.classList.remove("active");
+        modal_block.style.display = 'none';
+    });
+
+    close_button.addEventListener('click', () => {
+        darkness.classList.remove("active");
+        modal_block.style.display = 'none';
+    });
+});
+
+
+const rozwin_btn = document.getElementById("rozwin_btn");
+const grid__block = document.getElementById("grid__block");
+const open_grid = document.getElementById("open_grid");
 
 rozwin_btn.addEventListener("click", () => {
     grid__block.classList.toggle("active");
     open_grid.classList.toggle("active");
     let hasActive = rozwin_btn.classList.toggle("active");
     if(hasActive){
-        // rozwin_btn.textContent = "Zamknąć";
         rozwin_btn.innerHTML = 'Zamknąć &uarr;';
     }else{
-        // rozwin_btn.textContent = "Rozwiń";
         rozwin_btn.innerHTML = 'Rozwiń &darr;';
     }
 });
